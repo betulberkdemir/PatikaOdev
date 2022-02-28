57f3827edfda4845ea25e3e9868a312ac3fbca17
zehra-betul <100030147+zehra-betul@users.noreply.github.com>
2022-02-28 14:06:25 +0300

    Add files via upload

---
+++ polindrom.java
@@ -0,0 +1,36 @@
+package patika;
+
+public class polindrom {
+	
+	
+	static boolean isPolindrom(int number) {
+		
+		int temp = number,lastNumber, reverseNumber=0;
+		//345
+		while(temp != 0) {
+			
+			lastNumber = temp%10;
+			
+			
+			reverseNumber = (reverseNumber*10) + lastNumber;
+			
+			
+			temp /= 10;
+			
+		}
+		
+		if(number == reverseNumber)
+			return true;
+		else
+			return false;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(isPolindrom(121));
+		
+
+	}
+
+}
